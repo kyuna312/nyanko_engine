9825edefa4cfe193b824256a234ce4a92d0237f5
Khatanzorig <khatanzorig.b@tavanbogd.com>
2024-09-24 00:34:29 +0800

    it's first try on glfw module to draw component on rust

--- engine_tester/src/main.rs
+++ engine_tester/src/main.rs
@@ -1,3 +1,17 @@
+use nyanko_engine::graphics::window::Window;
+use gl::types::*;
+
 fn main() {
-    println!("Hello, world!");
+  let mut window = Window::new(800, 600, "Nyanko Engine");
+  
+  window.init_gl();
+
+    while !window.should_close() {
+        unsafe { 
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+        window.update();
+    }
+  
 }
---
+++ engine_tester/target/debug/deps/engine_tester-078ac9110dca2108.d
@@ -0,0 +1,5 @@
+/Users/maririn312/Desktop/nyanko_engine/engine_tester/target/debug/deps/engine_tester-078ac9110dca2108: src/main.rs
+
+/Users/maririn312/Desktop/nyanko_engine/engine_tester/target/debug/deps/engine_tester-078ac9110dca2108.d: src/main.rs
+
+src/main.rs:
